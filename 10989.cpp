661133d924d381aa797c29a3ec0ea70801e181c7
LeeJeyun <101978016+LeeJeyun@users.noreply.github.com>
2023-02-04 21:20:53 +0900

    10989 try1

---
+++ 10989.cpp
@@ -0,0 +1,27 @@
+// 수 정렬하기 
+#include <iostream>
+using namespace std;
+
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+   
+   int n;
+   cin >> n;
+
+   // counting the number of indexs.
+   int count[10001] = {0};
+   for (int i = 1; i <= n; i++){
+      int input = 0;
+      cin >> input;
+      count[input]++;
+   }
+   //printing indexs as many as the value of the indexs.
+   for (int i = 1; i <= 10000; i++){
+      for(int j = 0; j<count[i]; j++){
+         cout << i << endl;
+      }
+   }
+   return 0;
+}
--- 2750.cpp
+++ 2750.cpp
@@ -1,3 +1,4 @@
+// 수 정렬하기
 #include <iostream>
 #include <cmath>
 #include <list>
