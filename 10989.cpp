dcdbb07c24fd5deaa088815f14d417e6042e34bd
LeeJeyun <101978016+LeeJeyun@users.noreply.github.com>
2023-02-04 21:32:02 +0900

    10989 try3

--- 10989.cpp
+++ 10989.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 using namespace std;
 
-/** try 2
- * not using 
-ios::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
-*/
+/** try 3
+ * 
+using cout << "\n"
+*/ 
 
 int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
 
    int n;
    cin >> n;
@@ -24,7 +25,7 @@ int main(){
    //printing indexs as many as the value of the indexs.
    for (int i = 1; i <= 10000; i++){
       for(int j = 0; j < count[i]; j++){
-         cout << i << endl;
+         cout << i << "\n";
       }
    }
    return 0;
