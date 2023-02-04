98b3975496f546dcbb62a3cc4b0ef7ba65cc182a
LeeJeyun <101978016+LeeJeyun@users.noreply.github.com>
2023-02-04 21:49:01 +0900

    Create 1269.cpp

---
+++ 1269.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   // creating set A and set B
+   int n_a, n_b;
+   cin >> n_a >> n_b;
+   
+   int arr_a[n_a]={0};
+   for (int i = 0; i < n_a; i++){
+      int input;
+      cin >> input;
+      arr_a[i] = input;
+   }
+
+   int arr_b[n_b]={0};
+   for (int i = 0; i < n_b; i++){
+      int input;
+      cin >> input;
+      arr_b[i] = input;
+   }
+   
+   // 
+   // finding the number of (A - B)
+   int only_A = 0;
+   int only_B = 0;
+   for (int i = 0; i < n_a; i++){
+      arr_a[i]
+   }
+   return 0;
+}
