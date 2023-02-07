44c4c84625cfba7bfaaebfb5b43f6581515b0b57
jeyun02 <101978016+jeyun02@users.noreply.github.com>
2023-02-07 14:27:55 +0900

    1269 update

--- 1269.cpp
+++ 1269.cpp
@@ -18,27 +18,25 @@ int main(){
    for (int i = 0; i < n_a; i++){
       cin >> A[i];
    }
-
+   // int B[n_b];
+   int input;
+   int both = 0;
+   int search_start_index = 0;
    // input set B and make A to the unique set of A and B.
    for (int i = 0; i < n_b; i++){
-      int input;
+      // cin >> B[i];
       cin >> input;
-      
-      if( input < A[0] || input > A[-1] ) {
-         count ++;
-         continue;
+      int j =0;
+      while ( input >= A[j] && j < n_a){
+         if( input == A[j] ){
+            both++;
+            break;
+         }
+         j++;
       }
-      for ( int j =0; j<n_a; j++){
-         
-      }
-      int j = 0;
-      while( input != A[j] && j < n_a) j++;
-      count++;
-      
-
    }
 
    // output
-   cout << count;
+   cout << n_a + n_b - 2 * both;
    return 0;
 }
