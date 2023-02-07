7c014d92b189e3ec8fe6128bbd6ce6f260a6c919
jeyun02 <101978016+jeyun02@users.noreply.github.com>
2023-02-07 15:09:17 +0900

    1269 correct

--- 1269.cpp
+++ 1269.cpp
@@ -1,42 +1,19 @@
 // 대칭 차집합
 #include <iostream>
-
+#include <map>
 using namespace std;
-
+map<int, bool> m;
+int N,M;
+int num;
 int main(){
-   ios::sync_with_stdio(false);
-   cin.tie(NULL);
-   cout.tie(NULL);
-
-   // creating set A and set B
-   int n_a, n_b;
-   int count = 0;
-   cin >> n_a >> n_b;
-   int A[n_a];
-   
-   // input set A
-   for (int i = 0; i < n_a; i++){
-      cin >> A[i];
-   }
-   // int B[n_b];
-   int input;
-   int both = 0;
-   int search_start_index = 0;
-   // input set B and make A to the unique set of A and B.
-   for (int i = 0; i < n_b; i++){
-      // cin >> B[i];
-      cin >> input;
-      int j =0;
-      while ( input >= A[j] && j < n_a){
-         if( input == A[j] ){
-            both++;
-            break;
-         }
-         j++;
-      }
-   }
-
-   // output
-   cout << n_a + n_b - 2 * both;
-   return 0;
+	cin >> N >> M;
+	for (int i = 0; i < N+M; i++)
+	{
+		cin >> num;
+		if (m[num] == true) // 이미 존재하면 map에서 제거를 한다.
+			m.erase(num);
+		else // 존재 안하면 추가를 해준다.
+			m[num] = true;
+	}
+	cout << m.size(); // 남은 map 사이즈를 출력
 }
