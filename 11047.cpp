8067175ec1be779269dc5874951b2677de00503e
jeyun02 <101978016+jeyun02@users.noreply.github.com>
2023-02-15 18:43:03 +0900

    upload 11047

--- 11047.cpp
+++ 11047.cpp
@@ -1,1 +1,25 @@
-sdjfl
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int N, K;
+
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+   
+   cin >> N >> K;
+   int unit[N];
+   int count = 0;
+   for( int i = 0; i < N; i++){
+      cin >> unit[i];
+   }
+   for( int i = N-1; i >= 0; i--){
+      if( K >= unit[i]){
+         count += floor(K / unit[i]);
+         K = K % unit[i];
+      }
+   }
+   cout << count;
+}
