c8008545d7756ddbfc68e0363eab2549df57e71c
jeyun02 <101978016+jeyun02@users.noreply.github.com>
2023-02-15 16:19:10 +0900

    update 9461

--- 9461.cpp
+++ 9461.cpp
@@ -3,9 +3,9 @@ using namespace std;
 #define MAX 102
 
 int T;
-int arr[MAX] = {0,1,1,1};
-// int max_index;
-int Padovan(int N){
+long arr[MAX] = {0,1,1,1};
+
+long Padovan(int N){
    if (N > 3) {
       for( int i = 4; i <= N; i++ ){
          arr[i] = arr[i-2] + arr[i-3];
@@ -13,17 +13,17 @@ int Padovan(int N){
    }
    return arr[N];
 }
+
 int main(){
    ios::sync_with_stdio(false);
    cin.tie(NULL);
    cout.tie(NULL);
    
    cin >> T;
-   // max_index = 4;
+
    while(T--){
       int N;
       cin >> N;
       cout << Padovan(N)<< "\n";
-      // max_index = N;
    }
 }
