2f0c0a372a6c5b56d27875111f79a5252054e609
jeyun02 <101978016+jeyun02@users.noreply.github.com>
2023-02-17 14:39:40 +0900

    10828, 1931

--- 1931.cpp
+++ 1931.cpp
@@ -1,16 +1,33 @@
-#include <iostream>
-#include <cmath>
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
 using namespace std;
+int value[10];
+int main()
+{
+	int N, end, begin;
+	vector<pair<int, int>> schedule;
 
-int N;
+	cin >> N ;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> begin >> end;
+		// end, begin -> 종료 시각을 기준으로 정렬하기 위해
+		schedule.push_back(make_pair(end, begin));
 
-int main(){
-   cin >> N;
-   int arr[2*N];
-
-   for(int i = 0; i < N; i++ ){
-      cin >> arr[i] >> arr[i + N];
-   }
-
-   return 0;
+	}
+	// vector의 첫번째부터 마지막까지를 오름차순 정렬.
+	sort(schedule.begin(), schedule.end());
+	int time = schedule[0].first;
+	int count = 1;
+	for (int i = 1 ;i < N; i++) 
+	{
+		if (time <= schedule[i].second )
+		{
+			count++;
+			time = schedule[i].first;
+		}
+	}
+	cout << count;
 }
